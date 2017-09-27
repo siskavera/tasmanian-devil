091bb7f7cbb1a7622737ea85b372fbdaca751141
Veronika Siska <sskvera@gmail.com>
2017-09-27 14:47:58 +0100

    Added explanation to maths

--- model/GetEqDist.m
+++ model/GetEqDist.m
@@ -7,7 +7,7 @@ function [prop, population] = GetEqDist(deathRate, birthRate, ageTransferConst)
 
 nAgeClasses = length(deathRate);
 if ( nargin < 3 )
-    ageTransferConst = ones(1,nAgeClasses);
+    ageTransferConst = ones(1,nAgeClasses); % Yearly age-classes
 end
 
 population = zeros(size(deathRate));
@@ -15,11 +15,17 @@ alpha = population;
 
 alpha(1) = 1;
 for i = 2:(nAgeClasses - 1)
+    % Ratio between age classes converges to ratio of input and output
+    % rates
     alpha(i) = alpha(i-1)*ageTransferConst(i-1)/(ageTransferConst(i) + deathRate(i));
 end
 alpha(end) = alpha(end-1)*ageTransferConst(end-1)/deathRate(end);
 
-population(1) = (1 - (ageTransferConst(1) + deathRate(1))/sum(birthRate.*alpha))/sum(alpha);
+% From input = output for p0
+%   birth rate     carrying capacity  death   aging
+% sum_i(p_i b_i) * (1 - sum_i(p_i)) - (d_0 + age_0) * p_0 = 0
+% and p_i = alpha_i * p_0
+population(1) = (1 - (ageTransferConst(1) + deathRate(1))/sum(birthRate.*alpha) ) / sum(alpha);
 population = alpha.*population(1);
 
 prop = sum(population);
